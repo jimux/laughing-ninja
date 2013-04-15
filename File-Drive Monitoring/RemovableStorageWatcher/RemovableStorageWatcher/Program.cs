d5629fa25b99628f3c3a96056a24c5ce9474220b
James Smyth <jimsmyth@gmail.com>
2013-04-15 19:19:10 -0400

    Script name specified as arg. Comments. Some checks and set .NET to 2.0.

--- File-Drive Monitoring/RemovableStorageWatcher/RemovableStorageWatcher/Program.cs
+++ File-Drive Monitoring/RemovableStorageWatcher/RemovableStorageWatcher/Program.cs
@@ -2,24 +2,38 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace RemovableStorageWatcher
 {
     class Program
     {
-        private static List<char> activeDrives = new List<char>();
+        private static List<char> activeDrives = new List<char>(); // List of currently already known removable drives.
+		private static string script = @"newdrive.bat"; // Script to run when a new drive is detected.
 
         static void Main(string[] args)
         {
-            Console.WriteLine("----------------------------");
+			// "newdrive.bat" no good? Pass in a filename or whole path.
+			if(args.Length == 1)
+			{
+				script = args[0];
+			}
+
+			// If the given script doesn't exist, bail out. Nothing to 
+			if(!new FileInfo(script).Exists)
+			{
+				Console.WriteLine("Script " + script + " could not be found.");
+				Thread.Sleep(5000); // Keep the window open for five seconds, so there's time to see the error.
+				return;
+			}
+
             while (true)
             {
+				Thread.Sleep(1000); // Poll every one second. No need to hammer the system.
                 DriveInfo[] allDrives = DriveInfo.GetDrives();
 
+				// Gather a list of currently active drives.
                 var newActiveDrives = new List<char>();
                 foreach (DriveInfo drinfo in allDrives)
                 {
@@ -29,6 +43,7 @@ namespace RemovableStorageWatcher
                     }
                 }
 
+				// Compare with the previous list.
                 foreach (char letter in newActiveDrives)
                 {
                     bool isNewAvailable = true;
@@ -42,12 +57,11 @@ namespace RemovableStorageWatcher
                         }
                     }
 
+					// This is a new drive! Kick off the script.
                     if (isNewAvailable)
                     {
                         Console.WriteLine("New drive available: " + letter + @":\");
                         RunScript(letter);
-                        Console.WriteLine("Finished processing drive");
-                        Console.WriteLine("----------------------------");
                     }
                 }
 
@@ -57,10 +71,9 @@ namespace RemovableStorageWatcher
 
         private static void RunScript(char drive)
         {
-            Thread.Sleep(5000);
             Process proc = new Process();
-            proc.StartInfo.FileName = @"newdrive.bat";
-            proc.StartInfo.Arguments = drive.ToString();
+            proc.StartInfo.FileName = script;
+            proc.StartInfo.Arguments = drive.ToString(); // Only the drive letter itself is passed.
             proc.StartInfo.RedirectStandardError = true;
             proc.StartInfo.RedirectStandardOutput = true;
             proc.StartInfo.UseShellExecute = false;
